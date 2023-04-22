0340a885a06ccec77cd0ec1ca059c99f6611217c
Finn Gladstone <Finngladstone9@gmail.com>
2023-04-23 01:41:32 +1000

    Add (test) regex

--- pe_trader.h
+++ pe_trader.h
@@ -6,4 +6,8 @@
 #define BUFFER_SIZE 128
 #define PATH_LEN 25
 
+/* regex */
+#define SELL_SYNTAX "^MARKET SELL ([a-zA-Z]+) (\d+) (\d+);$"
+// #define SELL_SYNTAX "^MARKET SELL ([a-zA-Z]+) (-?\d+) (-?\d+);$"
+
 #endif
