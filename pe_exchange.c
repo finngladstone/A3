eecf40752d2ebe3549f6ca61787c289aa431b73e
Finn Gladstone <Finngladstone9@gmail.com>
2023-04-24 17:23:47 +1000

    Endgame pseudo

--- pe_exchange.c
+++ pe_exchange.c
@@ -3,11 +3,18 @@
 int main(int argc, char const *argv[])
 {
     
-   /* Control flow
+   /* Start-up 
     * 1. Read product file 
     * 2. Create named pipes for each trader (>= n)
     * 3. Launch trader as child process, assign trader ID starting from 0
     * 4. After launching each binary, exchange and trader connect to FIFO
+    */
+
+   /* Endgame
+    * - Print [SPX] Trader <Trader ID> disconnected
+    * - Reject pending / concurrent orders, maintain existing
+    * - [SPX] Trading completed
+    * - [SPX] Exchange fees collected: $<total fees>
     */
 
     return 0;
