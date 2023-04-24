66a2632106d7efb4a42850d3735072bb68c49cf7
Finn Gladstone <Finngladstone9@gmail.com>
2023-04-24 16:59:22 +1000

    Added exchange framework

---
+++ pe_exchange.c
@@ -0,0 +1,7 @@
+#include "pe_exchange.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ pe_exchange.h
@@ -0,0 +1,6 @@
+#ifndef PE_EXCHANGE_H
+#define PE_EXCHANGE_H
+
+#include <pe_common.h>
+
+#endif 
