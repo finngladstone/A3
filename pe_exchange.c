09749cd971cde4a431a245217dfec1059686484f
Finn Gladstone <Finngladstone9@gmail.com>
2023-04-24 17:21:54 +1000

    Add pseudocode

--- pe_exchange.c
+++ pe_exchange.c
@@ -2,6 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    
+   /* Control flow
+    * 1. Read product file 
+    * 2. Create named pipes for each trader (>= n)
+    * 3. Launch trader as child process, assign trader ID starting from 0
+    * 4. After launching each binary, exchange and trader connect to FIFO
+    */
+
     return 0;
 }
